71ecee6cfcd671b22f709d5c299a788add23b168
Alejandro Reyes <aalejandro.mreyes@icloud.com>
2021-06-17 15:38:49 -0400

    Fixes rubocop offenses

--- config/initializers/sentry.rb
+++ config/initializers/sentry.rb
@@ -1,5 +1,5 @@
 Sentry.init do |config|
-  config.dsn = 
+  config.dsn = ENV['SENTRY_URL']
   config.breadcrumbs_logger = [:active_support_logger, :http_logger]
 
   # Set tracesSampleRate to 1.0 to capture 100%
@@ -7,7 +7,7 @@ Sentry.init do |config|
   # We recommend adjusting this value in production
   config.traces_sample_rate = 0.5
   # or
-  config.traces_sampler = lambda do |context|
+  config.traces_sampler = ->() {
     true
-  end
+  }
 end
